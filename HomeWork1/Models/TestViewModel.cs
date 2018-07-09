2eebaeb067a53461b53d76267b2f289b695dba45
wayne <tw0988519746@hotmail.com>
2018-07-09 22:07:23 +0800

    增加資料庫EDMX與建立Metadata Type

---
+++ HomeWork1/Models/TestViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork1.Models
+{
+    public class TestViewModel
+    {
+
+        public string 客戶名稱 { get; set; }
+
+        public int 聯絡人數量 { get; set; }
+
+        public int 銀行帳戶數量 { get; set; }
+        
+    }
+}
