5a8f00ba30c559180bb7af3a242df8e9d1ec6e4c
wayne <tw0988519746@hotmail.com>
2018-07-25 21:32:58 +0800

    增加套版頁面

---
+++ HomeWork1/Models/BatchUpdateContactVM.cs
@@ -0,0 +1,24 @@
+﻿using HomeWork1.Models.InputValidate;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork1.Models
+{
+    public class BatchUpdateContactVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string 職稱 { get; set; }
+
+        [Required]
+        [PhoneNumber]
+        public string 手機 { get; set; }
+
+        [Required]
+        public string 電話 { get; set; }
+    }
+}
